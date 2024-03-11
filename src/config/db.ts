469e835c23d5b03f50d5fda7e530a287dc50d6dc
Vadym <vadym_v@ukr.net>
2024-03-11 17:01:15 +0200

    connect to db

---
+++ src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const { DB_NAME, DB_USER, DB_PASS } = process.env;
+
+const URI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.1jhtlp1.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log('MongoDB connected 🚀');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
